75e7010103f60ecb23e5064943329c1ee106d7cc
Andrea Manzi <manzolo@libero.it>
2018-09-01 19:01:22 +0200

    Added spid.configuredSPClient service to have an instance of sp already configured

--- Controller/SecurityController.php
+++ Controller/SecurityController.php
@@ -4,7 +4,6 @@
 namespace Italia\SpidSymfonyBundle\Controller;
 
 
-use Italia\Spid\Sp;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,23 +12,10 @@ class SecurityController extends Controller
 {
     public function metadataAction()
     {
-        $response = new Response($this->getSPClientInstance()->getSPMetadata());
+        $sp = $this->get("spid.configuredSPClient");
+        $response = new Response($sp->getSPMetadata());
         $response->headers->set('Content-Type', 'xml');
         return $response;
-    }
-
-    public function acsAction(Request $request)
-    {
-        $sp = $this->getSPClientInstance();
-
-        if ($sp->isAuthenticated()) {
-            $out = "";
-            foreach ($sp->getAttributes() as $key => $attr) {
-                $out .= $key . ' - ' . $attr . '<br>';
-            }
-            return new Response($out);
-        }
-        throw new \Exception('Unauthenticated');
     }
 
     public function chooseidpAction(Request $request)
@@ -39,7 +25,7 @@ class SecurityController extends Controller
 
     public function loginAction(Request $request, $idp)
     {
-        $sp = $this->getSPClientInstance();
+        $sp = $this->get("spid.configuredSPClient");
         /**
          * FIXME: waiting for upstream to fix the issue in the library
          */
@@ -51,25 +37,5 @@ class SecurityController extends Controller
     public function singleLogoutAction(Request $request)
     {
         throw new \Exception('Uninmplemented');
-    }
-
-    /**
-     * @return Sp
-     */
-    private function getSPClientInstance(): Sp {
-        $base = $this->container->getParameter('spid.sp_entityid');
-        $configuration = [
-            'sp_entityid' => $this->container->getParameter('spid.sp_entityid'),
-            'sp_key_file' => $this->container->getParameter('spid.sp_key_file'),
-            'sp_cert_file' => $this->container->getParameter('spid.sp_cert_file'),
-            'sp_assertionconsumerservice' => $this->container->getParameter('spid.sp_assertionconsumerservice'),
-            'sp_singlelogoutservice' => $this->container->getParameter('spid.sp_singlelogoutservice'),
-            'sp_org_name' => $this->container->getParameter('spid.sp_org_name'),
-            'sp_org_display_name' => $this->container->getParameter('spid.sp_org_display_name'),
-            'idp_metadata_folder' => $this->container->getParameter('spid.idp_metadata_folder').DIRECTORY_SEPARATOR,
-            'sp_attributeconsumingservice' => $this->container->getParameter('spid.sp_attributeconsumingservice'),
-        ];
-
-        return new Sp($configuration);
     }
 }
